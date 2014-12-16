ce68b59de94b08443ce58489947d26e75b76b391
Christian Perfect <christianperfect@gmail.com>
2014-12-16 14:27:37 +0000

    weight random letter choices

--- js/index.js
+++ js/index.js
@@ -12,11 +12,51 @@ $(document).ready(function() {
 		}
 	}
 
-	function choice(list) {
-		return list[randomint(0,list.length)];
+	function choice(list,weights) {
+		if(weights) {
+			var r = Math.random();
+			var acc =0;
+			for(var i=0;i<weights.length;i++) {
+				acc += weights[i];
+				if(acc>=r) {
+					return list[i];
+				}
+			}
+		} else {
+			return list[randomint(0,list.length)];
+		}
 	}
 
 	var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+	var weights = [
+		0.08167,
+		0.01492,
+		0.02782,
+		0.04253,
+		0.12702,
+		0.02228,
+		0.02015,
+		0.06094,
+		0.06966,
+		0.00153,
+		0.00772,
+		0.04025,
+		0.02406,
+		0.06749,
+		0.07507,
+		0.01929,
+		0.00095,
+		0.05987,
+		0.06327,
+		0.09056,
+		0.02758,
+		0.00978,
+		0.02360,
+		0.00150,
+		0.01974,
+		0.00074
+	];
+	console.log(weights.reduce(function(a,b){return a+b},0));
 
 	function wordsearch(words,size,directions) {
 		words = words.map(function(word){ return word.toUpperCase();});
@@ -82,7 +122,7 @@ $(document).ready(function() {
 			var x = pos[0], 
 					y = pos[1];
 			if(!grid[y][x]) {
-				grid[y][x] = choice(alphabet);
+				grid[y][x] = choice(alphabet,weights);
 			}
 		})
 		return grid;
